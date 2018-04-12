7b1801480e39f11f9ef15044d4818b73f1e04b68
Asif Rahman <asif0531@live.com>
2018-04-13 02:22:31 +0600

    angular route and register page designed

--- QuizApp-frontend/src/app/app.module.ts
+++ QuizApp-frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,6 +9,7 @@ import { RegisterComponent } from './register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ResultComponent } from './result/result.component';
+import { appRoutes } from './route';
 
 
 @NgModule({
@@ -18,7 +21,9 @@ import { ResultComponent } from './result/result.component';
     ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes), 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
